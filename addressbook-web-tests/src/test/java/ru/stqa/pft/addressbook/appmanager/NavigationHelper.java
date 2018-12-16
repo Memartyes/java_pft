fd5a3020ea3ebe6f6ac6d70b6df6f9d217bad577
Memartyes <memartyes@gmail.com>
2018-12-17 00:13:19 +0300

    _changes

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationHelper {
+
+    private WebDriver driver;
+
+    public NavigationHelper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void gotoGroupCreation() {
+        driver.findElement(By.linkText("groups")).click();
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTest extends TestBase {
+
+    @Test
+    public void testGroupCreation() throws Exception {
+        app.getNavigationHelper().gotoGroupCreation();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("Test", "test-header", "test-footer"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
